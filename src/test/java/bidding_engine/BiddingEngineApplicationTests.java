596ea90996ed1664d36cc7e29d41e6526cb87647
rubeshnpl13 <nepalnishanta29@gmail.com>
2026-08-27 01:56:42 +0545

    Initial Spring WebFlux bidding engine setup

---
+++ src/test/java/bidding_engine/BiddingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package bidding_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
